dde7105ab79c727582cdff90bc755f08871d3e43
Eschrich, Steven <steven.eschrich@moffitt.org>
2021-11-20 11:13:57 -0500

    Initial support for word document reports.

---
+++ R/qsc_docx.R
@@ -0,0 +1,9 @@
+#' qsc_docx template function
+#'
+#' See \code{\link[dsreportr]{docx}} for details.
+#'
+#'
+#' @export
+qsc_docx <- function() {
+  dsreportr::docx("qsctemplates::qsc_docx")
+}
---
+++ R/u54_docx.R
@@ -0,0 +1,9 @@
+#' u54_docx template function
+#'
+#' See \code{\link[dsreportr]{docx}} for details.
+#'
+#'
+#' @export
+u54_docx <- function() {
+  dsreportr::docx("qsctemplates::u54_docx")
+}
